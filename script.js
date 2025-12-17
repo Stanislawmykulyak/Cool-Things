2241dbc5ff55a788dd085fc93fccc2acd8bd8f9d
ShadyShade74 <shadyshade74@gmail.com>
2025-12-17 22:10:26 +0100

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const archer = document.getElementById('archer');
+const archerstats = document.querySelector('.archer-stats');
+archer.addEventListener('click', () => {
+    archerstats.classList.toggle('hidden')
+})
