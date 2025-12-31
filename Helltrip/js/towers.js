cf4a31dc78ed291994c9abc0edf3ea502e0ce989
ShadyShade74 <shadyshade74@gmail.com>
2025-12-31 17:56:35 +0100

    .

--- Helltrip/js/towers.js
+++ Helltrip/js/towers.js
@@ -1,48 +1,51 @@
-//Tower Template //
 class Sprite {
-    constructor({position = {x:0 , y:0} , imageSrc , frames = { max : 1 }}){
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
+    constructor({ position = { x: 0, y: 0 }, imageSrc, frames = { max: 1 }, offset = { x: 0, y: 0 } }) {
+        this.position = position;
+        this.image = new Image();
+        if (imageSrc) {
+            this.image.src = imageSrc;
+        }
         this.frames = {
             max: frames.max,
-            current:0,
-            elapsed:0,
-            hold:10,
-        }
+            current: 0,
+            elapsed: 0,
+            hold: 5,
+        };
+        this.offset = offset;
     }
-    
+
     draw() {
-        const cropWidth = this.image.width / this.frames.max
-        const crop ={
-            position : {
-                x:cropWidth * this.frames.current,
-                y:0,
+        if (!this.image.src) return;
+        const cropWidth = this.image.width / this.frames.max;
+        const crop = {
+            position: {
+                x: cropWidth * this.frames.current,
+                y: 0
             },
-            width:cropWidth,
-            height:this.image.height,
-        }
+            width: cropWidth,
+            height: this.image.height
+        };
         c.drawImage(
-            this.image , 
-            crop.position.x ,
-            crop.position.y ,
+            this.image,
+            crop.position.x,
+            crop.position.y,
             crop.width,
             crop.height,
-            this.position.x ,
-            this.position.y,
-            crop.width ,
-            crop.height,
-        )
-
-        this.frames.elapsed++
-        if(this.frames.elapsed % this.frames.hold === 0){
-
-        this.frames.current ++
-        if(this.frames.current >= this.frames.max - 1 ){
-            this.frames.current = 0
-        }
+            this.position.x + this.offset.x,
+            this.position.y + this.offset.y,
+            crop.width,
+            crop.height
+        );
     }
 
+    update() {
+        this.frames.elapsed++;
+        if (this.frames.elapsed % this.frames.hold === 0) {
+            this.frames.current++;
+            if (this.frames.current >= this.frames.max) {
+                this.frames.current = 0;
+            }
+        }
     }
 }
 class Tower {
